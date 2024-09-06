e4abb3cb05a0a906328ac6af4d3dacf554f2fed5
Grace <truequ33nofsquids@gmail.com>
2024-09-06 16:58:19 -0500

    additions
    added Test suff

---
+++ Test.java
@@ -0,0 +1,8 @@
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Blondie is fluffy");
+		System.out.println("Blondie is cute");
+		System.out.println("Blondie is a good girl");
+		System.out.println("Blondie has a snoot");
+	}
+}
