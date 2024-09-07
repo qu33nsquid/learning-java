5d269ed79704adba801af8e3c93c7fdfcc56bcb7
Grace <truequ33nofsquids@gmail.com>
2024-09-07 07:54:02 -0500

    true/false

--- Test.java
+++ Test.java
@@ -9,5 +9,12 @@ public class Test {
 	int num =  4;
 	String note = "P.S I's is ";
 	System.out.println(note + num);
+	float noom = 3.14159f;
+	String anotherNote = "P.SS I's learned the entirety of pi! It is ";
+	System.out.println(anotherNote + noom);
+	boolean isBlondiecute = true;
+	boolean isBlondiesmart = false;
+	System.out.println(isBlondiecute);
+	System.out.println(isBlondiesmart);
 	}
 }
